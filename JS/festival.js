c78681adc492bd0589588f8de7e65d7628324321
7jjin <wlsgud6305@naver.com>
2023-07-20 16:58:39 +0900

    지역 홍보 영상

---
+++ JS/festival.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".swiper", {
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+    clickable: true,
+  },
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
